6af29a58a07f21509cc9d6e7359049ffdfff50d3
Rtxshop <alialigolipourgolipour1387@gmail.com>
2026-09-22 02:16:46 +0330

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// ===== تنظیمات Supabase =====
+const SUPABASE_URL = 'https://ejmutrkvjmlrqmbpoj.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_7dbhi1-j01Cba_KFk6rxnw_fjmg3Ekm';
+const BUCKET_NAME = 'account-images';
