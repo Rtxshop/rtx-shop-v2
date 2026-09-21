cbbc1d127f52a23088175cc7c2bd52f9bce3f705
Rtxshop <alialigolipourgolipour1387@gmail.com>
2026-09-22 02:39:02 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-// ===== تنظیمات Supabase =====
-const SUPABASE_URL = 'https://ejmutrkvjmlrqmbpoj.supabase.co';
+const SUPABASE_URL = 'https://ejjmutrkvgjmlrqmbpoj.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_7dbhi1-j01Cba_KFk6rxnw_fjmg3Ekm';
 const BUCKET_NAME = 'account-images';
